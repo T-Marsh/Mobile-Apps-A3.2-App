38210a24b3512a98b61e872174f95f23752e5f78
T-Marsh <thomassjmarsh@gmail.com>
2019-05-31 01:05:38 +1000

    misc changes

--- app/src/main/java/assistant/genuinecoder/s_assistant/main/profile/StudentRegistration.java
+++ app/src/main/java/assistant/genuinecoder/s_assistant/main/profile/StudentRegistration.java
@@ -24,7 +24,7 @@ public class StudentRegistration extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_student__registartion);
+        setContentView(R.layout.activity_student__registration);
 
         spinner = (Spinner) findViewById(R.id.spinner);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, AppBase.divisions);
